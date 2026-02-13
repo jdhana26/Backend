b847f5dfa995bb45c48476a12711582a27047fef
Dhanavel <dhanavel426@gmail.com>
2026-02-13 06:54:16 +0530

    express

---
+++ Node/express/day32-backend/server.js
@@ -0,0 +1,14 @@
+import express  from "express";
+import dotenv from 'dotenv'
+
+
+const app = express()
+
+dotenv.config()
+
+const PORT=process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+
+    console.log(`Server connected Successfully http://localhost:${PORT}`);
+})
